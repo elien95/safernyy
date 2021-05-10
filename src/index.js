63908834713e957dd52827c75ba34dae6ba52923
elien <elien9omar@yaoo.com>
2021-05-10 03:36:35 +0300

    5 time

--- src/index.js
+++ src/index.js
@@ -1,64 +1,16 @@
 
 import './scss/styles.scss';
 import './css/style.css';
-//import '../node_modules/@laylazi/bootstrap-rtl-scss/scss/bootstrap-rtl.scss';
 import 'bootstrap-v4-rtl/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import '@fortawesome/fontawesome-free/js/all.min';
-import 'jquery.animate';
-import 'jquery'
-//$('.main-btn').click(function(){
-//window.location.herf='./src/tours.html';
-//})
-//window.onload = function() {
-/*  $(window).scroll(function(){
-    if($(this).scrollTop()>=500){
-       $("#navBar").addClass("noTransparrent");
-     }else{
-       $("#navBar").removeClass("noTransparrent");
-     }
-*/
+//page index.html
   document.getElementById("myButtonne").onclick = function () {
     location.href = "toursEygpt.html";
 };
     document.getElementById("myButton").onclick = function () {
         location.href = "toursTourkey.html";
     };
-//};
-  document.getElementById("init-account").onclick=function(event){
-    document.getElementsByClassName("account-btn").select();
-  };
 
-  document.getElementById("init-accounts").onclick = function () {
-    location.href = "index.html";
-};
-document.getElementById("register-accountt").onclick = function () {
-  location.href = "registerinto.html";
-};
-   /*
-    var check = function() {
-        if (document.getElementById('password').value ==
-          document.getElementById('confirm_password').value) {
-          document.getElementById('message').style.color = 'green';
-          document.getElementById('message').innerHTML = 'matching';
-        } else {
-          document.getElementById('message').style.color = 'red';
-          document.getElementById('message').innerHTML = 'not matching';
-        }
-      }*/
-      
-      var password=document.getElementById("password");
-      var confirm_password=document.getElementById("confirm_password");
-      var button_login=document.getElementById("register-account");
-     button_login.onclick=function(e){
-       if (confirm_password.value==password.value)
-       {
-         alert("ok");
-       }
-       else {
-         alert("password don't match")
-       }
-     }
-        
