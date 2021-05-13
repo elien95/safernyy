5ce406cadc34ab863d65ee76557447c2601da982
elien95 <82017747+elien95@users.noreply.github.com>
2021-05-13 09:37:01 +0300

    Add files via upload

---
+++ d1d6ee6434de3b25707f.js
@@ -0,0 +1,5 @@
+import 'bootstrap-v4-rtl/dist/css/bootstrap.min.css';
+import './css/style.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
